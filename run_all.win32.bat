66cfa6b953b0e404b5fd056f3dddd88727e48362
Lucien Murray-Pitts <lucien@rustoxide.com>
2014-03-03 03:20:29 +0900

    Fixes Unit Tests
    Unit tests didnt work properly on 64bit systems
    
    The reason turns out that the nunit console will auto-jit to 64bit but
    the binaries would be 32bit.
    
    Now forcing the x86, x64 mode compile in the binaries rather than
    leaving them as AnyCPU.
    
    Also forces Clean/Build so that the files actually get rebuilt for the
    unit tests
    
    Forces nunit console 32bit for the 32bit tests

--- run_all.win32.bat
+++ run_all.win32.bat
@@ -1,11 +1,16 @@
+
+rem set PATH=%PATH%;C:\Program Files (x86)\NUnit 2.6.3\bin
+
 erase tests\*.dll
 cd Core\KeraLua
 call Makefile.Win32.bat
-msbuild KeraLua.Net40.sln /p:Configuration=Release /p:Platform="Any CPU"
+msbuild KeraLua.Net40.sln /p:Configuration=Release /p:Platform="Any CPU" /p:PlatformTarget=x86 /t:Clean,Build
 cd ..\..
 xcopy Core\KeraLua\external\lua\win32\bin\*.dll tests\*.dll
-msbuild NLua.Net40.sln /p:Configuration=Release /p:Platform="Any CPU"
+msbuild NLua.Net40.sln /p:Configuration=Release /p:Platform="Any CPU" /p:PlatformTarget=x86 /t:Clean,Build
 cd tests/
-nunit-console NLuaTest.dll /xml=$1
+
+rem Force JITS to 32bit on 32/64 bit CPU
+nunit-console-x86 NLuaTest.dll /xml=$1
 cd ..
 
--- run_all.win64.bat
+++ run_all.win64.bat
@@ -1,10 +1,15 @@
+
+rem set PATH=%PATH%;C:\Program Files (x86)\NUnit 2.6.3\bin
+
 erase tests\*.dll
 cd Core\KeraLua
 call Makefile.Win64.bat
-msbuild KeraLua.Net40.sln /p:Configuration=Release /p:Platform="Any CPU"
+msbuild KeraLua.Net40.sln /p:Configuration=Release /p:PlatformTarget=x64 /t:Clean,Build
 cd ..\..
 xcopy Core\KeraLua\external\lua\win64\bin64\*.dll tests\*.dll
-msbuild NLua.Net40.sln /p:Configuration=Release /p:Platform="Any CPU"
+msbuild NLua.Net40.sln /p:Configuration=Release /p:PlatformTarget=x64 /t:Clean,Build
 cd tests/
+
+rem This JITS to 64bit on 64 CPU, 32bit on 32 CPU
 nunit-console NLuaTest.dll /xml=$1
 cd ..
